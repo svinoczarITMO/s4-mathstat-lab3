5dd64b97e6941b57c0fd692e51fdcc0ffb6862ac
svinoczar <alexsupper75@gmail.com>
2024-03-13 18:03:19 +0300

    finish task2

--- task2.py
+++ task2.py
@@ -1,34 +1,33 @@
 import pandas as pd
-import matplotlib.pyplot as plt
 import numpy
+import matplotlib.pyplot as plt
+import seaborn as sns
 import pretty_print
 
-iris = pd.read_csv('iris.csv')
 
-setosa = iris[iris['Species'] == 'setosa']
-versicolor = iris[iris['Species'] == 'versicolor']
-virginica = iris[iris['Species'] == 'virginica']
+iris = pd.read_csv('iris.csv')
 
 sepal_length = 'Sepal.Length'
 sepal_width = 'Sepal.Width'
 petal_length = 'Petal.Length'
 petal_width = 'Petal.Width'
 
-iris_sepal_square = iris[sepal_length]*iris[sepal_width]
-iris_petal_square = iris[petal_length]*iris[petal_width]
-setosa_sepal_square = setosa[sepal_length]*setosa[sepal_width]
-setosa_petal_square = setosa[petal_length]*setosa[petal_width]
-versicolor_sepal_square = versicolor[sepal_length]*versicolor[sepal_width]
-versicolor_petal_square = versicolor[petal_length]*versicolor[petal_width]
-virginica_sepal_square = virginica[sepal_length]*virginica[sepal_width]
-virginica_petal_square = virginica[petal_length]*virginica[petal_width]
+iris['sepal_square'] = iris[sepal_length] * iris[sepal_width]
+iris['petal_square'] = iris[petal_length] * iris[petal_width]
+iris['total_square'] = iris['sepal_square'] + iris['petal_square']
+
+setosa = iris[iris['Species'] == 'setosa']
+versicolor = iris[iris['Species'] == 'versicolor']
+virginica = iris[iris['Species'] == 'virginica']
 
 def sample_average(square):
     return sum(square)/square.count()
 
+
 def sample_variance(square, avg):
     avg_square = avg * avg
     return sum(square ** 2) / square.count() - avg_square
+
 
 def sample_median(square):
     sorted_square = square.sort_values()
@@ -37,12 +36,13 @@ def sample_median(square):
         return (sorted_square.iloc[count//2 - 1] + sorted_square.iloc[count//2])/2
     else:
         return sorted_square.iloc[count//2 - 1]
+
     
 def calculations():
     data_map = {}
-    species = {"Total": [iris_sepal_square, iris_petal_square], "Setosa": [setosa_sepal_square, setosa_petal_square],
-               "Versicolor": [versicolor_sepal_square, versicolor_petal_square], "Virginica": [virginica_sepal_square, virginica_petal_square]}
-
+    species = {"Total": [iris['sepal_square'], iris['petal_square']], "Setosa": [setosa['sepal_square'], setosa['petal_square']],
+               "Versicolor": [versicolor['sepal_square'], versicolor['petal_square']], "Virginica": [virginica['sepal_square'], virginica['petal_square']]}
+    
     for spec in species:
         sepal_square, petal_square = species[spec][0], species[spec][1]    
         sepal_sample_avg, petal_sample_avg = round(sample_average(sepal_square), 4), round(sample_average(petal_square), 4)
@@ -52,12 +52,62 @@ def calculations():
         data_map[spec] = [str(sepal_sample_avg), str(petal_sample_avg), sepal_sample_var, petal_sample_var, sepal_sample_median, petal_sample_median, sepal_sample_quantile, petal_sample_quantile]
 
     return data_map
-4
-def output():
+
+def tables_output():
     values = calculations()
+    species_count = {"Setosa": len(setosa), "Versicolor": len(versicolor), "Virginica": len(virginica)}
     data = [["SPECIES", "SEPAL AVERAGE", "PETAL AVERAGE", "SEPAL VARIANCE", "PETAL VARIANCE", "SEPAL MEDIAN", "PETAL MEDIAN", "SEPAL QUANTILE", "PETAL QUANTILE"]]
+    min_max_data = [["SPECIES", "COUNT"]]
+    
     for spec in values:
         data.append([spec] + values[spec])
+        
+    for spec in species_count:
+        count = [str(species_count[spec])]
+        min_max_data.append([spec] + count)
+    
+    print("\n")
+    pretty_print.print_pretty_table(min_max_data)
+    print("\n\n")
     pretty_print.print_pretty_table(data)
+    print("\n")
 
-output()
+def plot_all():
+    fig, axes = plt.subplots(nrows=1, ncols=3, figsize=(15, 5))
+    total_square = 'Total Square (Sepal + Petal)'
+
+    # ECDF
+    sns.ecdfplot(iris['total_square'], label='Total', color='black', ax=axes[0])
+    sns.ecdfplot(setosa['total_square'], label='Setosa', color='blue', ax=axes[0])
+    sns.ecdfplot(versicolor['total_square'], label='Versicolor', color='red', ax=axes[0])
+    sns.ecdfplot(virginica['total_square'], label='Virginica', color='purple', ax=axes[0])
+    axes[0].set_title('Empirical Cumulative Distribution Function (ECDF)')
+    axes[0].set_xlabel(total_square)
+    axes[0].set_ylabel('Cumulative Probability')
+    axes[0].legend()
+    axes[0].grid()
+
+    # Histogram
+    sns.histplot(iris['total_square'], kde=False, color='black', label='Total', ax=axes[1])
+    sns.histplot(setosa['total_square'], kde=False, label='Setosa', color='blue', ax=axes[1])
+    sns.histplot(versicolor['total_square'], kde=False, label='Versicolor', color='red', ax=axes[1])
+    sns.histplot(virginica['total_square'], kde=False, label='Virginica', color='purple', ax=axes[1])
+    axes[1].set_title('Histogram of Total Square (Sepal + Petal)')
+    axes[1].set_xlabel(total_square)
+    axes[1].set_ylabel('Frequency')
+    axes[1].legend()
+    axes[1].grid()
+
+    # Box-plot
+    sns.boxplot(x='Species', y='total_square', data=iris, hue='Species', palette=['blue', 'red', 'purple'], legend=False, ax=axes[2])
+    axes[2].set_title('Box-plot of Total Square (Sepal + Petal)')
+    axes[2].set_xlabel('Species')
+    axes[2].set_ylabel(total_square)
+    axes[2].grid()
+
+    plt.tight_layout()
+    plt.show()
+
+if __name__ == '__main__':
+    tables_output()
+    plot_all()
