03a2761e64ff6f873fe9219d9dffa3d60da45f34
Roman <romankrohin126@gmail.com>
2024-04-11 22:37:20 +0300

    Correct task2

--- Lab2/task2.py
+++ Lab2/task2.py
@@ -1,21 +1,30 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy.stats import norm
 
-def grade(samples, n):
-    thetas, bias, vars, mses = [0] * n, [0] * n, [0] * n, [0] * n
-    for i in range(n):
-        thetas[i] = (np.sqrt(3*np.var(samples[i])))
-        bias[i] = abs(thetas[i] - 10)
-        vars[i] = abs(np.var(samples[i]) - np.var(samples, ddof=1))
-        mses[i] = np.mean((thetas[i]-10)**2)
-    mle_thetas = [thetas[i] for i in range(n)]
-    return thetas, bias, vars, mses, tuple(mle_thetas)
+data_samples = np.random.normal(loc=0, scale=1, size=100)
 
-mse_mles = []
-for n in range(100, 1001, 100):
-    samples = np.random.uniform(low=-10, high=10, size=(n, 100))
-    thetas, bias, vars, mses, mle_thetas = grade(samples, n)
-    
-    mse_mles.append(np.mean((mle_thetas - 10)**2))
-    
-print("MSE for MLE estimates: ", np.mean(mse_mles))
+def likelihood(theta, data):
+    likelihoods = [norm.pdf(x, loc=theta, scale=1) for x in data]
+    return np.prod(likelihoods)
+
+def prior(theta):
+    return norm.pdf(theta, loc=0, scale=1)
+
+def posterior(theta, data):
+    return likelihood(theta, data) * prior(theta)
+
+def normalize_posterior(theta_range, data):
+    unnormalized_posterior = [posterior(theta, data) for theta in theta_range]
+    normalization_constant = np.trapz(unnormalized_posterior, theta_range)
+    normalized_posterior = unnormalized_posterior / normalization_constant
+    return normalized_posterior
+
+def bayesian_estimate(theta_range, posterior):
+    return np.trapz(theta_range * posterior, theta_range)
+
+theta_range = np.linspace(-10, 10, 1000)
+
+normalized_posterior = normalize_posterior(theta_range, data_samples)
+
+bayesian_theta_estimate = bayesian_estimate(theta_range, normalized_posterior)
+print("Байесовская оценка параметра theta:", bayesian_theta_estimate)
