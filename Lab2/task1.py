806c7866629fe2f92e2ece4ef7085426fbbfc1a6
Roman <romankrohin126@gmail.com>
2024-04-18 00:32:44 +0300

    task1 fix

--- Lab2/task1.py
+++ Lab2/task1.py
@@ -1,12 +1,21 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot_theta_distribution(thetas, n):
+    plt.figure(figsize=(10, 6))
+    plt.hist(thetas, bins=20, density=True, alpha=0.6)
+    plt.xlabel('Значение параметра $\\theta$')
+    plt.ylabel('Плотность вероятности')
+    plt.title(f'Распределение оценок параметра $\\theta$ при выборках размера {n}')
+    plt.grid(True)
+    plt.show()
+
 def grade(samples, n):
     thetas, bias, vars, mses = [0] * n, [0] * n, [0] * n, [0] * n
     for i in range(n):
         thetas[i] = (np.sqrt(3*np.var(samples[i])))
         bias[i] = abs(thetas[i] - 10)
-        vars[i] = abs(np.var(samples[i]) - np.var(samples, ddof=1))
+        vars[i] = np.var(thetas)
         mses[i] = np.mean((thetas[i]-10)**2)
     return thetas, bias, vars, mses
 
@@ -14,28 +23,17 @@ def grade(samples, n):
 for n in range(100, 1001, 100):
     samples = np.random.uniform(low=-10, high=10, size=(n, 100))
     thetas, bias, vars, mses = grade(samples, n)
+    out_of_range = []    
+    for i in thetas:
+        if np.abs(np.abs(i) - 10)>0.88:
+            out_of_range.append(i)
+    print("За границей:")
+    print (out_of_range[:10])
+    print("Сдвиги:")
+    print (bias[:10])
+    print("Дисперсии:")
+    print (vars[:10])
+    print("Среднеквадратичная ошибка:")
+    print (mses[:10])    
+    plot_theta_distribution(thetas, n)
     
-    print(np.mean(thetas))
-    
-    count_bias = []
-    count_vars = []
-    count_mses = []
-    
-    count_bias.append(sum(1 for value in bias if value > 0.3))
-    count_vars.append(sum(1 for value in vars if value > 0.5))
-    count_mses.append(sum(1 for value in mses if value > 0.05))
-
-    labels = ['']
-    x = np.arange(len(labels))
-    width = 0.25
-
-    plt.figure(figsize=(10, 6))
-    plt.bar(x - width, count_bias, width, color='blue', label='Bias')
-    plt.bar(x, count_vars, width, color='green', label='Variance')
-    plt.bar(x + width, count_mses, width, color='red', label='MSE')
-
-    plt.xlabel('Arrays')
-    plt.xticks(x, labels)
-    plt.legend()
-    plt.grid(True)
-    plt.show()
