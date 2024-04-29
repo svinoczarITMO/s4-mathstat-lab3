639f19b757e558cf5a1fa4dd632d2d41f3d6cf06
Roman <romankrohin126@gmail.com>
2024-04-29 22:40:08 +0300

    Fix task1

--- Lab3/task1.py
+++ Lab3/task1.py
@@ -5,51 +5,35 @@ mu_1 = 2
 mu_2 = 1
 sigma_1_squared = 1
 sigma_2_squared = 0.5
-n = 25
-m = 25
 alpha = 0.05
 repetitions = 1000
 
+real_tau = mu_1 - mu_2
+sample_sizes = [25, 10000]
+
 def confidence_interval(X, Y, alpha):
-    mean_X = np.mean(X)
-    mean_Y = np.mean(Y)
-    
-    std_diff = np.sqrt(sigma_1_squared/n + sigma_2_squared/m)
-    
-    standardized_diff = (mean_X - mean_Y - (mu_1 - mu_2)) / std_diff
-    
+    n1 = len(X)
+    n2 = len(Y)
+
+    std_diff = np.sqrt(sigma_1_squared / n1 + sigma_2_squared / n2)
+    standardized_diff = (np.mean(X) - np.mean(Y) - real_tau) / std_diff
     z_alpha = norm.ppf(1 - alpha / 2)
-    
-    lower_bound = standardized_diff - z_alpha * std_diff
-    upper_bound = standardized_diff + z_alpha * std_diff
-    
+    lower_bound = standardized_diff - z_alpha
+    upper_bound = standardized_diff + z_alpha
     return lower_bound, upper_bound
 
-def check_coverage(lower_bound, upper_bound):
-    return 0 >= lower_bound and 0 <= upper_bound
+def experiment(mu1, mu2, alpha, sample_size, experiments):
+    covered = 0
+    for _ in range(experiments):
+        X = np.random.normal(mu1, np.sqrt(sigma_1_squared), sample_size)
+        Y = np.random.normal(mu2, np.sqrt(sigma_2_squared), sample_size)
+        interval = confidence_interval(X, Y, alpha)
+        if interval[0] <= 0 <= interval[1]:
+            covered += 1
+    coverage_probability = covered / experiments
+    return coverage_probability
 
-covered_count_25 = 0
-for _ in range(repetitions):
-    X = np.random.normal(mu_1, np.sqrt(sigma_1_squared), n)
-    Y = np.random.normal(mu_2, np.sqrt(sigma_2_squared), m)
-    lower_bound, upper_bound = confidence_interval(X, Y, alpha)
-    if check_coverage(lower_bound, upper_bound):
-        covered_count_25 += 1
-
-print("Для выборок объема 25:")
-print(f"Количество покрывающих интервалов: {covered_count_25}/{repetitions}")
-print(f"Процент покрытия: {covered_count_25 / repetitions * 100:.2f}%")
-
-n = 10000
-m = 10000
-covered_count_10000 = 0
-for _ in range(repetitions):
-    X = np.random.normal(mu_1, np.sqrt(sigma_1_squared), n)
-    Y = np.random.normal(mu_2, np.sqrt(sigma_2_squared), m)
-    lower_bound, upper_bound = confidence_interval(X, Y, alpha)
-    if check_coverage(lower_bound, upper_bound):
-        covered_count_10000 += 1
-
-print("\nДля выборок объема 10000:")
-print(f"Количество покрывающих интервалов: {covered_count_10000}/{repetitions}")
-print(f"Процент покрытия: {covered_count_10000 / repetitions * 100:.2f}%")
+for sample_size in sample_sizes:
+    coverage_probability = experiment(mu_1, mu_2, alpha, sample_size, repetitions)
+    print(f"Для выборки объемом {sample_size}:")
+    print(f"Вероятность покрытия реального значения параметра: {coverage_probability}")
